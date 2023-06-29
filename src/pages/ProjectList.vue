30a24c459a55f04eeb33d359cbedd0d2d3dc22e6
LorenzoMegna24 <megnalorenzo0@gmail.com>
2023-06-29 15:08:29 +0200

    install route

---
+++ src/pages/ProjectList.vue
@@ -0,0 +1,45 @@
+<script >
+import axios from 'axios';
+import ProjectCardComp from './components/ProjectCardComp.vue';
+
+export default{
+
+    components:{
+        ProjectCardComp
+    },
+
+    data(){
+        return{
+            projects: [],
+            urlBase: 'http://127.0.0.1:8000'
+        }
+    },
+    mounted(){
+        this.getProjects();
+    },
+    methods: {
+        getProjects(){
+            axios.get(`${this.urlBase}/api/projects`).then(res=>{
+                this.projects = res.data.projects
+            })
+        }
+    },
+}
+
+</script>
+
+<template>
+ <h1>Projects</h1>
+
+    <main>
+        <div class="container">
+            <div class="row">
+                <ProjectCardComp v-for="(elem, index) in projects" :key="index" :proj="elem"/>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style lang="scss">
+@use '../style/main.scss'
+</style>
