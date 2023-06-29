b5584f26682ad5aa746b7f7b2a30c94556e5e17c
LorenzoMegna24 <megnalorenzo0@gmail.com>
2023-06-29 09:44:34 +0200

    Fix card img

--- src/components/ProjectCardComp.vue
+++ src/components/ProjectCardComp.vue
@@ -5,7 +5,8 @@ export default{
     props: ['proj'],
     data(){
       return{
-        
+        urlBase: 'http://127.0.0.1:8000'
+
       }
     }
   }
@@ -15,7 +16,7 @@ export default{
 <template>
         <div class="col-lg-6">
             <div class="card">
-                <img class="card-img-top" :src="`${urlBase}/storage/${proj.img}`" alt="Title">
+                <img class="card-img-top" :src="`${urlBase}/storage/${proj.img}`" alt="project">
                 <div class="card-body">
                     <h4 class="card-title">{{ proj.project_title }}</h4>
                     <p class="card-text">{{ proj.description }}</p>
