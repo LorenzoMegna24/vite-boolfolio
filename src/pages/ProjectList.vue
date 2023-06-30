8bfa31d939e04d7f124241ab7f00cdef12c4ab70
LorenzoMegna24 <megnalorenzo0@gmail.com>
2023-06-30 16:27:29 +0200

    Creazione del filtro

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -10,16 +10,31 @@ export default{
     data(){
         return{
             projects: [],
-            urlBase: 'http://127.0.0.1:8000'
+            types: [],
+            urlBase: 'http://127.0.0.1:8000',
+            selectedType: 'all'
         }
     },
     mounted(){
         this.getProjects();
+        this.getTypes();
     },
     methods: {
         getProjects(){
-            axios.get(`${this.urlBase}/api/projects`).then(res=>{
+
+            const params = ''
+
+           if (this.selectedType !== 'all') {
+                params.type_id = this.selectedType
+           }
+
+            axios.get(`${this.urlBase}/api/projects`,{params}).then(res=>{
                 this.projects = res.data.projects
+            })
+        },
+        getTypes(){
+            axios.get(`${this.urlBase}/api/types`).then(res =>{
+                this.types = res.data.types
             })
         }
     },
@@ -31,6 +46,14 @@ export default{
  <h1>Projects</h1>
 
     <main>
+        <div class="mb-3 container w-50">
+            <label for="" class="form-label">Types filter</label>
+
+            <select @change="getProjects()" v-model="selectedType" class="form-select form-select-lg" name="" id="">
+                <option value="all">--All--</option>
+                <option :value="elem.id" v-for="(elem, index) in types" :key="index">{{ elem.name_type }}</option>
+            </select>
+        </div>
         <div class="container">
             <div class="row">
                 <ProjectCardComp v-for="(elem, index) in projects" :key="index" :proj="elem"/>
