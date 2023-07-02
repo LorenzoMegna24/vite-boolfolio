198023980e5d6f2a61f6c3ee7f9f26fc3439e0e9
LorenzoMegna24 <megnalorenzo0@gmail.com>
2023-07-02 16:43:33 +0200

    Aggiunta pagination

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -12,24 +12,32 @@ export default{
             projects: [],
             types: [],
             urlBase: 'http://127.0.0.1:8000',
-            selectedType: 'all'
+            selectedType: 'all',
+            currentPage: 1,
+            lastPage: null,
         }
     },
     mounted(){
-        this.getProjects();
+        this.getProjects(1);
         this.getTypes();
     },
     methods: {
-        getProjects(){
+        getProjects(projectApiPage){
 
-            const params = ''
+            // const params = ''
 
-           if (this.selectedType !== 'all') {
-                params.type_id = this.selectedType
-           }
+        //    if (this.selectedType !== 'all') {
+        //         params.type_id = this.selectedType
+        //    }
 
-            axios.get(`${this.urlBase}/api/projects`,{params}).then(res=>{
-                this.projects = res.data.projects
+            axios.get(`${this.urlBase}/api/projects`,{
+                params: {
+                    page: projectApiPage
+                }
+            }).then(res=>{
+                this.projects = res.data.projects.data
+                this.currentPage = res.data.projects.current_page
+                this.lastPage = res.data.projects.last_page
             })
         },
         getTypes(){
@@ -59,6 +67,23 @@ export default{
                 <ProjectCardComp v-for="(elem, index) in projects" :key="index" :proj="elem"/>
             </div>
         </div>
+        <nav aria-label="Page navigation">
+          <ul class="pagination    ">
+            <li class="page-item">
+              <a class="page-link" @click.prevent="getProjects(currentPage - 1)" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+              </a>
+            </li>
+            <li class="page-item" :class="(currentPage === elem) ? 'active' : ''" aria-current="page" v-for="(elem, index) in lastPage" :key="index">
+                <a class="page-link" @click.prevent="getProjects(elem)" href="#">{{elem}}</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" @click.prevent="getProjects(currentPage + 1)" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
     </main>
 </template>
 
